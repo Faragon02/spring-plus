2f9296aad74afe4d3eba1796fda9043011d13aa5
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-05-09 13:11:19 +0900

    fix: lv1-5 코드 개선 퀴즈 - AOP의 이해 feat: @EnableAspectJAutoProxy 추가

--- src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
@@ -4,8 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -17,14 +17,20 @@ import java.time.LocalDateTime;
 public class AdminAccessLoggingAspect {
 
     private final HttpServletRequest request;
-
-    @After("execution(* org.example.expert.domain.user.controller.UserController.getUser(..))")
+    //lv1-5 코드 개선 퀴즈 - AOP의 이해
+    @Before("execution(* org.example.expert.domain.user.controller.UserController.getUser(..))")
     public void logAfterChangeUserRole(JoinPoint joinPoint) {
-        String userId = String.valueOf(request.getAttribute("userId"));
-        String requestUrl = request.getRequestURI();
-        LocalDateTime requestTime = LocalDateTime.now();
+        String role = String.valueOf(request.getAttribute("userRole"));
+        if("ADMIN".equals(role)){
+            String userId = String.valueOf(request.getAttribute("userId"));
 
-        log.info("Admin Access Log - User ID: {}, Request Time: {}, Request URL: {}, Method: {}",
+            String requestUrl = request.getRequestURI();
+            LocalDateTime requestTime = LocalDateTime.now();
+
+            log.info("Admin Access Log - User ID: {}, Request Time: {}, Request URL: {}, Method: {}",
                 userId, requestTime, requestUrl, joinPoint.getSignature().getName());
+
+        }
+
     }
 }
